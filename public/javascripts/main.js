7843ee6bfbc3add0c7da982cfba755fc4ced5ba9
Fab Mackojc <fab.mackojc@gmail.com>
2013-12-15 17:34:43 -0800

    Use Angular app namespace and extract out total value method

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,16 @@
-function MainCtrl($scope){
-  $scope.wallet = {btc: 0, ltc: 0}
-  $scope.usd_to = {btc: 1014, ltc: 34}
-}
+var ngApp = angular.module('coinfolio', [])
+
+ngApp.controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.wallet = { btc : 0, ltc : 0 }
+    $scope.usdTo = { btc : 1014, ltc : 34 }
+
+    $scope.totalValue = function (wallet, exchangeRates) {
+      var total = 0
+      for (var currency in exchangeRates) {
+        var rate = exchangeRates[currency]
+        total += wallet[currency] * rate
+      }
+      return total
+    }
+  }
+])
