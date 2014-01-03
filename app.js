8f29048f3b9ec291e33c77c9c8ec72a31dd99e99
Fab Mackojc <fab.mackojc@gmail.com>
2014-01-03 16:37:17 -0700

    Extract exchange rate update code into seperate file

--- app.js
+++ app.js
@@ -6,14 +6,12 @@
 var express = require('express')
 var routes = require('./routes')
 var user = require('./routes/user')
+var currency = require('./routes/currency')
 var http = require('http')
 var path = require('path')
 var ejs  = require('ejs')
-var request = require('request')
 
 var app = express()
-
-var exchangeRates = {}
 
 // all environments
 app.set('port', process.env.PORT || 3000)
@@ -35,31 +33,9 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index)
 app.get('/users', user.list)
-app.get('/rates', function(req, res) {
-  res.send(exchangeRates)
-})
+app.get('/rates', currency.rates)
 
-function getExchangeRates () {
-  var baseURL = 'http://cryptocoincharts.info/v2/api/tradingPairs'
-    , currencies = ['btc', 'ltc', 'ppc']
-
-  var reqString = currencies.map(function (currency) {
-    return currency += '_usd'
-  }).join(',')
-
-  request.post(baseURL, { form: { pairs : reqString } }, function (err, response, body) {
-    var responseArray = JSON.parse(body)
-    responseArray.forEach(function (currencyData) {
-      var currencyName = currencyData.id.substr(0, 3)
-        , lastPrice = currencyData.price
-
-      exchangeRates[currencyName] = lastPrice
-    })
-  })
-}
-
-getExchangeRates()
-setInterval(getExchangeRates, 60000)
+currency.updateExchangeRates()
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'))
